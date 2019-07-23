2b163f120c4b31fc2ba36001ad25ca39fe10d03e
sumersao <saosumer@gmail.com>
2019-07-23 00:36:37 -0700

    Fast Expo

---
+++ src/FastExpo.java
@@ -0,0 +1,12 @@
+public class FastExpo {
+	public static long pow(long x, long y, long p) {
+		long res = 1;     
+		x = x % p; 
+		while (y > 0) {
+			if((y & 1)==1) res = (res * x) % p;
+			y = y >> 1; 
+			x = (x * x) % p; 
+		}
+		return res;
+	}
+}
